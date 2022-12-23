6ac340582c92de74572b45d62c05babd8f5272af
Sanju Kamat <sanjukamat123@gmail.com>
2022-12-23 11:15:43 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.1502183582939516,
+        "pitch": 0.5710706533428755,
+        "fov": 1.2430790330847488
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04144669488880659,
+          "pitch": 0.02833084816877296,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.7824238436586928,
+        "pitch": 0.42827283193840593,
+        "fov": 1.2430790330847488
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.43233259364000887,
+          "pitch": 0.07437337719676762,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -3.0200921775261964,
+          "pitch": 0.24940616725574571,
+          "rotation": 0,
+          "target": "0-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.5366795101045518,
+        "pitch": 0.2724695171079965,
+        "fov": 1.2430790330847488
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6619650896441813,
+          "pitch": 0.11478537000536448,
+          "rotation": 0,
+          "target": "0-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Gym",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
